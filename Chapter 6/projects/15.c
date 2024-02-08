fdcc08ec7c9eb57e5cb5495ffc875289751629ad
alexm356 <alexmondroski@Alexs-MacBook-Pro.local>
2024-02-07 18:08:47 -0800

    update

--- Chapter 6/projects/15.c
+++ Chapter 6/projects/15.c
@@ -4,27 +4,30 @@ int main(){
 
     double const rate = .10;
 
-    int year;
+    double const rate2 = .05;
 
-    double Daphne = 100.00 , Dierdre = 100.00;
+    int year = 1;
+
+    double Daphne = 100.00 , Dierdre = 100.01;
 
     double increase;
+
+    double increaseD;
+
+    increase = rate * Daphne;
 
 
     do {
 
-        increase = rate * Daphne;
+        printf("Year: %d\n", year);
+        Daphne += increase;
+        printf("Daphne investment: %f\n", Daphne);
+        Dierdre += Dierdre * rate2;
+        printf("Dierdre investment: %f\n", Dierdre);
+
+        year++;
 
 
-        printf("Daphne year %d", year);
-        printf("Dierdre year %d", year)
-
-
-
-    } while(Dierdre > Daphne)
-
-
-
-
+    } while (Daphne > Dierdre);
 
 }
