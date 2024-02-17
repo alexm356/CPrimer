8cf3bb2acea387ece5912538fa999f94dd15d356
alexm356 <alexmondro01@gmail.com>
2024-02-16 18:09:49 -0800

    exe

---
+++ Chapter 7/ex/10.c
@@ -0,0 +1,6 @@
+/*
+
+Rewrite the program in question 9 so that it exhibits the same behavior but does
+not use a continue or a goto.
+
+*/
---
+++ Chapter 7/ex/9.c
@@ -0,0 +1,26 @@
+//input : q
+//c
+//g
+//b
+
+#include <stdio.h>
+int main(void)
+{
+ char ch;
+ while ((ch = getchar()) != '#')
+ {
+ if (ch == '\n')
+ continue;
+ printf("Step 1\n");
+ if (ch == 'c')
+ continue;q
+ else if (ch == 'b')
+ break;
+ else if (ch == 'g')
+ goto laststep;
+ printf("Step 2\n");
+ laststep: printf("Step 3\n");
+ }
+ printf("Done\n");
+ return 0;
+}
