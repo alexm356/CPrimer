01b1d1a9a6a947ea775dcdf910d2c4c85429566a
alexm356 <alexmondro01@gmail.com>
2024-02-26 15:18:50 -0800

    update

--- Chapter 8/projects/7.c
+++ Chapter 8/projects/7.c
@@ -4,5 +4,4 @@ Modify exercise 8 from Chapter 7 so that the menu choices are labeled by
 characters instead of by numbers.
 
 
-
 */
--- Chapter 8/projects/8.c
+++ Chapter 8/projects/8.c
@@ -1,62 +1,134 @@
 #include <stdio.h>
 
+double getInput(void);
 
-double getfirst();
+char get_choice(void);
+char get_letter(void);
 
-double getSecond();
-
+void add();
+void subtract();
+void multiply();
+void divide();
 
 int main(){ 
 
     int choice;
 
-    int resultFirst;
-
-    int resultSecond;
-
-    while ((choice = )
-
-        switch(choice){
-
-
-            case 'a':
-
-                resultFirst = getFirst();
-                resultSecond = getSecond();
-
-
-
-
-            case 's':
-
-            case 'm':
-
-            case 'd':
-
-            case 'q':
-
-                break;
-
-            default: 
-
-                printf("Incorrect input!");
-
-
+    while ((choice = get_choice()) != 'q')
+    {
+        switch(choice)
+        {
+        case 'a':
+            add();
+            break;
+        case 's':
+            subtract();
+            break;
+        case 'm':
+            multiply();
+            break;
+        case 'd':
+            divide();
+            break;
+        default:
+            printf("Incorrect input!");
+            break;
         }
 }
+}
 
-double getFirst(){
+double getInput(void){
 
-    double first;
-
-    while ((scanf("%lf ", &first) != {
-
-        printf("Enter first number: ");
+    double input;
+    char ch;
+    while (scanf("%lf", &input) != 1)
+    {
+        while ((ch = getchar()) != '\n')
+            putchar(ch); // dispose of bad input
+        printf(" is not an integer.\nPlease enter an ");
+        printf("integer value, such as 25, -178, or 3: ");
+    }
+    return input;
     
 
 }
 
-double getSecond(){
+char get_choice(void){
+
+    int ch;
+
+    printf("Enter the letter of your choice:\n");
+    printf("a. add s. subtract\n");
+    printf("m. multiply d. multiply\n");
+    printf("q. quit\n");
+
+    ch = get_letter();
+
+    while (ch != 'q'){
+        if (ch == 'a' || ch =='s' || ch == 'm' || ch == 'd')
+        {
+            return ch;
+        }
+        else {
+            printf("Please respond with a, s, m, or d.\n");
+            ch = get_letter();
+        }
+    }
+
+}
+
+char get_letter(void){
+    int ch;
+    ch = getchar();
+    while (getchar() != '\n')
+        continue;
+    return ch;
+}
+
+void add(){
+
+    double first, second;
+
+    printf("Enter first number: ");
+        first = getInput();
+    printf("Enter second number: ");
+        second = getInput();
+    printf("%.2lf + %.2lf = %.2lf\n", first, second, first + second);
+
+}
+
+void subtract(){
+
+    double first, second;
+
+    printf("Enter first number: ");
+        first = getInput();
+    printf("Enter second number: ");
+        second = getInput();
+    printf("%.2lf - %.2lf = %.2lf\n", first, second, first - second);
+
+}
+
+void multiply(){
+
+    double first, second;
+
+    printf("Enter first number: ");
+        first = getInput();
+    printf("Enter second number: ");
+        second = getInput();
+    printf("%.2lf * %.2lf = %.2lf\n", first, second, first * second);
+
+}
+
+void divide(){
 
 
+    double first, second;
+
+    printf("Enter first number: ");
+        first = getInput();
+    printf("Enter second number: ");
+        second = getInput();
+    printf("%.2lf / %.2lf = %.2lf\n", first, second, first / second);
 }
